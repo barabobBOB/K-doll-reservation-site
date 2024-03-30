88b1d3e78c5700c22aa0d985085c8ac653d38c7c
Seyeon_Choi <choi20014830@gmail.com>
2024-03-31 06:39:14 +0900

    set: 스프링부트 프로젝트 생성

---
+++ ticket/src/test/java/com/rena/ticket/TicketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rena.ticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
