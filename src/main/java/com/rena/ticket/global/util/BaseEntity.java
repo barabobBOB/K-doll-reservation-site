a089a34b04eaf489254eabd670a79ed3f7e4e29e
choiseyeon <choi20014830@gmail.com>
2024-04-16 03:10:29 +0900

    feat: BaseEntity를 추가합니다.

---
+++ src/main/java/com/rena/ticket/global/util/BaseEntity.java
@@ -0,0 +1,53 @@
+package com.rena.ticket.global.util;
+
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.Where;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+@Getter
+@SuperBuilder
+@MappedSuperclass
+@Where(clause = "deleted_at is null")
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    @Column(updatable = false, name = "created_at")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+
+    public boolean isDeleted() {
+        return null != this.deletedAt;
+    }
+
+    public void delete() {
+        this.deletedAt = ZonedDateTime.now(ZoneId.of("UTC")).toLocalDateTime();
+    }
+
+    public void undoDelete() {
+        this.deletedAt = null;
+    }
+
+}
